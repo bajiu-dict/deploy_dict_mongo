daf012b90eb257ffe5b637e37d6667d28030c45d
pancongwen <peter.pancongwen@gmail.com>
2020-08-01 16:30:31 +0800

    feat: update env
    - add mongo init environment variables

--- env.sh
+++ env.sh
@@ -3,3 +3,8 @@ PASSWORD=turingmachine
 DB=es2cn
 AUTHDB=admin
 BACKUPDIR=backup
+
+# environment variables for mongodb docker
+MONGO_INITDB_DATABASE=admin
+MONGO_INITDB_ROOT_USERNAME=root
+MONGO_INITDB_ROOT_PASSWORD=abc123_
