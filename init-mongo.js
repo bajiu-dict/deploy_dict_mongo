cd00847964d52351d04ca5a4f9c112687c1f9da8
pancongwen <peter.pancongwen@gmail.com>
2020-08-07 01:56:16 +0800

    feat: update mongo init js script
    - create two databases

--- init-mongo.js
+++ init-mongo.js
@@ -1,4 +1,6 @@
 // comment auth in /etc/mongodb.conf
+
+db = db.getSiblingDB('es2cn');
 db.createUser(
   {
     user: "phoenix",
@@ -6,8 +8,23 @@ db.createUser(
     roles: [ 
       { 
         role: "dbOwner", 
-        db: "es2cn" } 
+        db: "es2cn" }
     ]
-
   }
-)
+);
+db.createCollection('test');
+
+
+db = db.getSiblingDB('dict_user');
+db.createUser(
+  {
+    user: "phoenix",
+    pwd: "turingmachine", // or cleartext password
+    roles: [ 
+      { 
+        role: "dbOwner", 
+        db: "dict_user" }
+    ]
+  }
+);
+db.createCollection('test');
