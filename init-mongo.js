26aaf8bf4803111de490b1f7846529fda3e101a0
pancongwen <peter.pancongwen@gmail.com>
2020-08-09 15:25:30 +0800

    fix: cann ot restore

--- init-mongo.js
+++ init-mongo.js
@@ -1,30 +1,24 @@
 // comment auth in /etc/mongodb.conf
 
-db = db.getSiblingDB('es2cn');
-db.createUser(
+admin_db = db.getSiblingDB('admin');
+admin_db.createUser(
   {
     user: "phoenix",
     pwd: "turingmachine", // or cleartext password
     roles: [ 
       { 
         role: "dbOwner", 
-        db: "es2cn" }
-    ]
-  }
-);
-db.createCollection('test');
-
-
-db = db.getSiblingDB('dict_user');
-db.createUser(
-  {
-    user: "phoenix",
-    pwd: "turingmachine", // or cleartext password
-    roles: [ 
-      { 
+        db: "es2cn" },
+      {
         role: "dbOwner", 
-        db: "dict_user" }
+        db: "dict_user"
+      }
     ]
   }
 );
-db.createCollection('test');
+
+dict_db = db.getSiblingDB('es2cn');
+dict_db.createCollection('test');
+
+user_db = db.getSiblingDB('dict_user');
+user_db.createCollection('test');
