60c9c85f42c1a596ec89ac661b2328e93bde4b08
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2020-11-23 11:37:34 +0800

    add index of word

---
+++ init_db_index.sh
@@ -0,0 +1,3 @@
+mongo en2cn --eval 'db.vocabs.createIndex({ word: 1})' --username phoenix --password turingmachine --authenticationDatabase admin
+mongo es2cn --eval 'db.vocabs.createIndex({ word: 1})' --username phoenix --password turingmachine --authenticationDatabase admin
+
