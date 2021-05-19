38d5aa72026700c37d2412b1ee8517b5e75223ee
JingWeiLyu <murka@JingWeiLyus-MBP.powter>
2021-05-19 22:16:08 +0800

    update db index creation

--- init_db_index.sh
+++ init_db_index.sh
@@ -1,5 +1,8 @@
 mongo en2cn --eval 'db.vocabs.createIndex({ word: 1})' --username dict --password turingmachine --authenticationDatabase admin
 mongo es2cn --eval 'db.vocabs.createIndex({ word: 1})' --username dict --password turingmachine --authenticationDatabase admin
-mongo es2cn --eval 'db.verbs_variation.createIndex({ word: 1})' --username dict --password turingmachine --authenticationDatabase admin
+
+mongo es2cn --eval 'db.verbs_extension.createIndex({ word: 1})' --username dict --password turingmachine --authenticationDatabase admin
 mongo es2cn --eval 'db.verbs_inverse_variation.createIndex({ word: 1})' --username dict --password turingmachine --authenticationDatabase admin
 
+mongo en2cn --eval 'db.sentences.createIndex({ original: "text"}, {default_language: "english"})' --username dict --password turingmachine --authenticationDatabase admin
+mongo es2cn --eval 'db.sentences.createIndex({ original: "text"}, {default_language: "spanish"})' --username dict --password turingmachine --authenticationDatabase admin
